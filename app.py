c5895fa1f7e35aaa19c2900cc1b04d71e2166eb3
rozumary <montesarose@gmail.com>
2025-11-29 00:23:20 +0800

    Fix app.py import path to find main.py in backend

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ Hugging Face Space entry point for iBrood app
 import os
 import sys
 
+# Add backend to path so we can import main
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "backend"))
+
 # Set environment variable to tell main.py we're in Hugging Face
 os.environ['HF_SPACE'] = '1'
 
